69476371a29f93830c72129e934f3b38f70e92fa
tGoh98 <tmg5@rice.edu>
2019-07-22 11:29:22 -0500

    Added delayed fadein transition for begin button

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,19 +7,42 @@ import { AwesomeButton } from 'react-awesome-button';
 import "./buttonStyles.css";
 
 
+class Button extends Component {
+	componentWillMount() {
+    var that = this;
+    this.setState({visibility: "hidden"});
+		setTimeout(function() {
+			that.show();
+		}, that.props.wait);
+	}
+	show() {
+		this.setState({visibility: "visible"});
+	}
+  render() {
+        return (
+          <div className="button" style={{visibility: this.state.visibility}}>
+              <AwesomeButton size={150} type="primary"><Link style={{ textDecoration: 'none', color: 'black' }} to='/home'>Begin</Link></AwesomeButton>
+          </div>
+        )
+    }
+};
+
+
 export default class Intro extends Component {
   render() {
     return (
-      <div className="centered">
-        <ParticleComponent />
-        <Typing className="header"
-                speed={300}
-                >
-          asdf
-          <Typing.Delay ms={10000} />
-        </Typing>
+      <div className="content">
         <div className="centered">
-          <AwesomeButton size={150} type="primary"><Link style={{ textDecoration: 'none', color: 'black' }} to='/home'>Begin</Link></AwesomeButton>
+          <ParticleComponent />
+          <Typing className="header"
+                  speed={100}
+                  >
+            Welcome
+            <Typing.Delay ms={2000} />
+          </Typing>
+          <div className="centered">
+            <Button wait={1100} />
+          </div>
         </div>
       </div>
     );
