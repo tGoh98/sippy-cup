cbad0788eeaa12aafbc88a9e744d8507df91c597
tGoh98 <tmg5@rice.edu>
2019-07-23 13:02:16 -0500

    Testing trivial ParticleComponent

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './Intro.css';
-// import ParticleComponent from './ParticleComponent';
+import ParticleComponent from './ParticleComponent';
 import Typing from 'react-typing-animation';
 import { AwesomeButton } from 'react-awesome-button';
-import MouseMug from './MouseMug';
+// import MouseMug from './MouseMug';
 import "./buttonStyles.css";
 import "react-awesome-button/dist/styles.css"
 
@@ -20,7 +20,7 @@ export default class Intro extends Component {
     return (
       <div className="content">
         <div className="centered">
-          {/*<ParticleComponent />*/}
+          <ParticleComponent />
           <Typing className="header"
                   speed={80}
 									onFinishedTyping={() => this.setState({showButton:true})}
@@ -31,7 +31,6 @@ export default class Intro extends Component {
           { this.state.showButton &&
 						<div className="centered fadein">
 						<AwesomeButton className="buttonBegin" type="primary"><Link className="linkText" to='/home'>Begin</Link></AwesomeButton>
-						<MouseMug />
 						</div>
 					}
         </div>
