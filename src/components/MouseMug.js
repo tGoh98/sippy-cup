5e015d0078901b75299fc9507df4cfbfb102647d
tGoh98 <tmg5@rice.edu>
2019-07-25 11:37:11 -0500

    Increased mug and font size

--- src/components/MouseMug.js
+++ src/components/MouseMug.js
@@ -4,8 +4,8 @@ import cup1 from '../assets/cup1.png';
 export default class MouseMug extends Component {
   render() {
     return (
-      <div style={{padding:0, left: this.props.posX-160, top: this.props.posY-90, position: 'absolute'}}>
-        <img src={ cup1 } style={{ maxWidth: 200 }} alt="mug"/>
+      <div style={{padding:0, left: this.props.posX-220, top: this.props.posY-110, position: 'absolute'}}>
+        <img src={ cup1 } style={{ maxWidth: 260 }} alt="mug"/>
       </div>
     );
   };
